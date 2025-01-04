1c95b5a3662b7a38d9ceffb0902bd795652a5a35
bakhadir <myblogfree1988@gmail.com>
2025-01-05 01:42:54 +0300

    test 2

--- src/shared/components/latest-certificates/index.tsx
+++ src/shared/components/latest-certificates/index.tsx
@@ -9,7 +9,7 @@ export const LatestCertificates = ({ data }: { data: LatestNewsDto }) => {
   return (
     <section className={styles.root}>
       <div className="container">
-        <h2>{data.title}</h2>
+        <h2>{data.title}123</h2>
         {data.content && (
           <div dangerouslySetInnerHTML={{ __html: data.content }} />
         )}
